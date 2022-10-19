2145b484d292a0c675c76d9dd60a62ab140715e6
mlimabasualdo <114433470+mlimabasualdo@users.noreply.github.com>
2022-10-18 19:39:12 -0500

    18OCT

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,85 +2,62 @@ import { Link } from "@remix-run/react";
 
 import { useOptionalUser } from "~/utils";
 
+
+
 export default function Index() {
   const user = useOptionalUser();
   return (
     <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
-      <div className="relative sm:pb-16 sm:pt-8">
+      <div className="relative sm:pb-8 sm:pt-4">
+      <a href="https://karmona.mx/">
+                <img
+                  src="https://github.com/mlimabasualdo/my-new-repo-for-gh-imagenes/blob/main/fac-logo-1589397452-45227.png?raw=true"
+                  alt="Karmona"
+                  className="mx-auto mt-2 w-full max-w-[2rem] md:max-w-[8rem]"
+                />
+              </a>
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
             <div className="absolute inset-0">
               <img
                 className="h-full w-full object-cover"
-                src="https://github.com/mlimabasualdo/my-new-repo-for-gh-imagenes/blob/main/Fondo%20principal.jpeg?raw=true"
+                src="https://github.com/mlimabasualdo/my-new-repo-for-gh-imagenes/blob/main/FondoTesla.jpg?raw=true"
                 alt="Karmona"
               />
-              <div className="absolute inset-0 bg-[color:rgba(190,208,244)] mix-blend-multiply" />
+              <div className="absolute inset-0 bg-[color:rgba(236,229,229,1)] mix-blend-multiply" />
             </div>
             <div className="relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pb-20 lg:pt-32">
-              <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
-                <span className="block uppercase text-blue-500 drop-shadow-md">
+              <h1 className="text-center text-4xl font-extrabold tracking-tight sm:text-4xl lg:text-4xl">
+                <span className="block uppercase text-white drop-shadow-md">
                   Jinetes del Acero
                 </span>
               </h1>
               <p className="mx-auto mt-6 max-w-lg text-center text-xl text-white sm:max-w-3xl">
-              Somos un centro de servicio automotriz profesional con más de 30 años de experiencia 
-              en la reparación y mantenimiento de vehículos. Siempre buscamos resolver los problemas 
-              de los autos de nuestros clientes y mantener a sus vehículos en perfectas condiciones. 
-              Todos los servicios mecánicos son realizados por un equipo profesional.
+              Somos un centro de servicio automotriz profesional con más de 30 años de experiencia en la reparación y mantenimiento de vehículos. Siempre buscamos resolver los problemas de los autos de nuestros
+               clientes y mantener a sus vehículos en perfectas condiciones. Todos los servicios mecánicos son realizados por un equipo profesional.
               </p>
-              <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
-                {user ? (
-                  <Link
-                    to="/notes"
-                    className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-white-700 shadow-sm hover:bg-blue-60 sm:px-8"
-                  >
-                    View Notes for {user.email}
-                  </Link>
-                ) : (
-                  <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
-                    <Link
-                      to="/join"
-                      className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-white-700 shadow-sm hover:bg-blue-60 sm:px-8"
-                    >
-                      Agenda tu Cita
-                    </Link>
-                    <Link
-                      to="/login"
-                      className="flex items-center justify-center rounded-md bg-blue-500 px-4 py-3 font-medium text-white hover:bg-blue-700"
-                    >
-                      Log In
-                    </Link>
-                  </div>
-                )}
-              </div>
-              <a href="https://karmona.mx/">
-                <img
-                  src="https://github.com/mlimabasualdo/my-new-repo-for-gh-imagenes/blob/main/fac-logo-1589397452-45227.png?raw=true"
-                  alt="Karmona"
-                  className="mx-auto mt-16 w-full max-w-[12rem] md:max-w-[16rem]"
-                />
-              </a>
+              
+              
             </div>
           </div>
         </div>
 
         <div className="mx-auto max-w-7xl py-2 px-4 sm:px-6 lg:px-8">
         <h1 className="text-center text-4xl font-extrabold tracking-tight sm:text-4xl lg:text-4xl">
-                <span className="block uppercase text-black-500 drop-shadow-md">
-                  ¿Como Trabajamos?
+                <span className="block uppercase text-black drop-shadow-md">
+                  Servicios
                 </span>
-                </h1>
+              </h1>
           <div className="mt-6 flex flex-wrap justify-center gap-8">
             {[
               {
-                src: "https://github.com/mlimabasualdo/my-new-repo-for-gh-imagenes/blob/main/Foto1.jpeg?raw=true",
-                alt: "Planeacion",
+                src: "https://user-images.githubusercontent.com/1500684/157764397-ccd8ea10-b8aa-4772-a99b-35de937319e1.svg",
+                alt: "Fly.io",
                 href: "https://fly.io",
               },
               {
-                src: "https://github.com/mlimabasualdo/my-new-repo-for-gh-imagenes/blob/main/Foto2.jpeg?raw=true",
-                alt: "Ejecucion",
+                src: "https://user-images.githubusercontent.com/1500684/157764395-137ec949-382c-43bd-a3c0-0cb8cb22e22d.svg",
+                alt: "SQLite",
                 href: "https://sqlite.org",
               },
               {
@@ -142,4 +119,4 @@ export default function Index() {
       </div>
     </main>
   );
-}
+            }
